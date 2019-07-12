e0e0e912ee099077f3377b627d5ba16a86eea65e
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-07-12 13:39:15 -0400

    add basic miner solution

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,6 +24,9 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
+
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,7 +41,10 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    last_hash = hashlib.sha256(f'{last_hash}'.encode()).hexdigest()
+    this_hash = hashlib.sha256(f'{proof}'.encode()).hexdigest()
+    return this_hash[:6] == last_hash[-6:]
+
 
 
 if __name__ == '__main__':
